69e1168fa0b31c3afd38421f3a29b0ce04b6218a
Ghanashyam Baviskar <48504443+ghanaxhyam@users.noreply.github.com>
2024-05-03 22:10:33 +0530

    Add files via upload

---
+++ advait.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Terminal 1
+osascript -e 'tell application "Terminal" to do script "/usr/local/bin/python3 /Users/advait/oneaboveall/backend/main.py"'
+
+# Terminal 2
+osascript -e 'tell application "Terminal" to do script "/usr/local/bin/python3 /Users/advait/oneaboveall/backend/server.py"'
+
+# Terminal 3 
+osascript -e 'tell application "Terminal" to do script "cd /Users/advait/oneaboveall/my-chatbot-frontend && npm start"' 
+#!/bin/bash
+
+
+CURRENT_HOUR=$(date +%H)
+
+if [ $CURRENT_HOUR -ge 5 ] && [ $CURRENT_HOUR -lt 12 ]; then
+    GREETING="Good morning ghanashyam! your The vhatbot is ready"
+elif [ $CURRENT_HOUR -ge 12 ] && [ $CURRENT_HOUR -lt 18 ]; then
+    GREETING="Good afternoon ghanashyam! your Chatbot is ready"
+else
+    GREETING="Good evening ghanashyam! your Chatbot is ready"
+fi
+
+# Voice notification
+osascript -e 'say "'"$GREETING"'"'
+
